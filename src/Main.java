b5d869cc5f9822d6c45a56ad036356dd9a48089f
neto study <kseniyalazare@yandex.ru>
2023-02-07 18:25:22 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 29750; // Стоимость билета, руб
+        int onemile = 20; // Количество рублей для одной бонусной мили
+
+
+        System.out.println("Количество бонусных миль:");
+        System.out.println(price / onemile);
+    }
+}
